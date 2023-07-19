44039c373846d7d3c0659ec073d45f4dbd643d68
Tullio Sebastiani <tsebasti@redhat.com>
2023-07-19 11:11:38 +0200

    added folder logic
    added folder logic
    
    
    added folder logic

--- app.py
+++ app.py
@@ -2,11 +2,13 @@
 import os
 import boto3
 import json
-import uuid
 from krkn_lib_kubernetes import ChaosRunTelemetry
 from flask import Flask, Response, request
 from typing import Optional
+
 app = Flask(__name__)
+
+request_id_param: str = "request_id"
 
 
 @app.route("/", methods=['POST'])
@@ -14,39 +16,45 @@ def telemetry():
     content_type = request.headers.get('Content-Type')
     if content_type == 'application/json':
         try:
+            query_params = request.args
+            if request_id_param not in query_params.keys():
+                return Response(
+                    "[bad request]: missing request_id param",
+                    status=400
+                )
+
+            folder_name = query_params.get(request_id_param)
             bucket_name = os.getenv("BUCKET_NAME")
             if bucket_name is None:
                 return Response("BUCKET_NAME env variable not set", status=500)
+
             telemetry_data = ChaosRunTelemetry(request.json)
             validation_response = validate_data_model(telemetry_data)
             # if validator returns means that there are validation errors
             if validation_response is not None:
                 return validation_response
-            uuid_str = str(uuid.uuid4())
-            file_name = f"{uuid_str}.json"
-            s_three = boto3.resource("s3")
+
+            s_three = boto3.resource('s3')
+            bucket = s_three.Bucket(bucket_name)
+
             telemetry_str = json.dumps(
                 telemetry_data, default=lambda o: o.__dict__, indent=4
             )
-            s_three.Bucket(bucket_name).put_object(Key=file_name, Body=telemetry_str)
-            return Response(f"record {uuid_str} created test")
+            bucket.put_object(
+                Key=f"{folder_name}/telemetry.json",
+                Body=telemetry_str
+            )
+            return Response(f"record {folder_name}/telemetry.json created")
         except Exception as e:
             return Response(f"[bad request]: {str(e)}", status=400)
     else:
         return Response("content type not supported", status=415)
 
 
-def validate_data_model(model:  ChaosRunTelemetry) -> Optional[Response]:
+def validate_data_model(model: ChaosRunTelemetry) -> Optional[Response]:
     for scenario in model.scenarios:
         for attr, _ in scenario.__dict__.items():
             if attr != "parametersBase64":
-                if getattr(scenario, attr) is None or getattr(scenario, attr) == "" :
+                if getattr(scenario, attr) is None or getattr(scenario, attr) == "":
                     return Response(f"[bad request]: {attr} is null or empty")
     return None
-
-
-
-
-
-
-
